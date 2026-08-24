575a80bbf1baa8d75d100d2efca0633095df6eff
admin <admin@admins-Mac-mini.local>
2026-08-24 08:30:01 -0700

    fix ios map

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR_GOOGLE_MAPS_API_KEY")
+    GMSServices.provideAPIKey("AIzaSyBS6dc6ntXcW8O8ZAdCMcvMigxttlEEHZ8")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
